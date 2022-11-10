ab2c6fb0c16f0fc3cb226458fa6f2b6a9ac95d42
Manisha <mpaswale@gmail.com>
2022-11-10 07:59:26 +0530

    New priject added

---
+++ STS_Project/src/main/java/com/example/StsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsProjectApplication.class, args);
+	}
+
+}
